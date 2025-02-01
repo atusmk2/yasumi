b5e7601d62d8c33315ced1ac1db47afa47b94414
atus <atusmk2@yahoo.com>
2025-02-01 11:51:01 +0700

    Changed +5 flat damage to +20% damage bonus per upgrade on non-summon items.

--- Items/yasumiUpgradeAtk.cs
+++ Items/yasumiUpgradeAtk.cs
@@ -2,19 +2,25 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 using System.Collections.Generic;
+using Terraria.Localization;
 using System.IO;
 using yasumi.Common;
+using Terraria.ModLoader.UI;
 
 namespace yasumi.Items
 {
-		public class yasumiUpgradeAttack : GlobalItem {
+	public class yasumiUpgradeAttack : GlobalItem {
 		public override bool InstancePerEntity => true;
 		public int damageplus;
-		internal int damUp;
+		public static LocalizedText Damagetext { get; private set;}
+        public override void SetStaticDefaults() {
+			Damagetext = Mod.GetLocalization("Damagetext");
+		}
+        internal int damUp;
 		public bool CheckWeapon(Item Item) {return (Item.stack == 1 && Item.damage > 0 && !Item.consumable && !Item.CountsAsClass(DamageClass.Summon)) && !Item.accessory;}
 		public bool DamUpgrader() {
 			if (Main.mouseItem.type == ModContent.ItemType<AttackUP>()) {
-				damUp = 5;
+				damUp = 1;
 				return true;
 			}
 			return false;
@@ -40,7 +46,7 @@ namespace yasumi.Items
 				Main.mouseItem.stack--;
 			}
 			if (CheckWeapon(Item) && Resetter()) {
-				player.QuickSpawnItem(player.GetSource_Misc("drop"), ModContent.ItemType<AttackUP>(), damageplus / 5);
+				player.QuickSpawnItem(player.GetSource_Misc("drop"), ModContent.ItemType<AttackUP>(), damageplus);
 				damageplus -= damageplus;
 				Item.stack++;
 				Main.mouseItem.stack--;
@@ -49,14 +55,14 @@ namespace yasumi.Items
 		public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
 		{
 			if (CheckWeapon(item) && damageplus > 0) {
-				var line = new TooltipLine(Mod, "yasumi", $"[i:{ModContent.ItemType<AttackUP>()}] [c/92f892:Damage +{damageplus * 4}%]");
+				var line = new TooltipLine(Mod, "yasumi", Damagetext.Format(damageplus * 20)+$"[i:{ModContent.ItemType<AttackUP>()}]");
 				tooltips.Add(line);
 			}
 		}		
 		public override void ModifyWeaponDamage(Item item, Player player, ref StatModifier damage)
 		{
 			if (damageplus > 0 && !item.CountsAsClass(DamageClass.Summon)) {
-				damage += (0.04f * damageplus);
+				damage += (0.2f * damageplus);
 			}
 		}
 		public override void SaveData(Item item, TagCompound tag)
